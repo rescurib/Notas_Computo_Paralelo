24dbc3f5e3bc44c2dd3f3e34875878463db97b35
Rodolfo E. Escobar U <orion_sigma@hotmail.com>
2020-08-03 00:11:12 -0500

    Update Ejemplo_3_2_OpenMP.c

--- MemoriaCompartida/Sec_03/Ejemplo_3_2_OpenMP.c
+++ MemoriaCompartida/Sec_03/Ejemplo_3_2_OpenMP.c
@@ -1,82 +1,71 @@
-#include<omp.h>
+#include <omp.h>
 #include<stdio.h>
 #include<stdlib.h>
-//#include <time.h>
-#include "leerBin.h"
+#include"leerBin.h"
 
-# define N 20
+void intercambiar(double *a, double *b){ 
+    double t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
 
-void mxv_serial(int m, int n, double *A, double *v, double *u);
-void mxv_paralela(int m, int n, double *A, double *v, double *u);
+//------ BubbleSort Serial -----
+int bubbleSort_Serial(double v[],int N){
+    int i,j;
+    for (i = 0; i < N-1; i++) 
+        for (j = 0; j < N-i-1; j++) 
+            if (v[j] > v[j+1]){
+                intercambiar(&v[j+1],&v[j]);                   
+            }
+    }
 
-int main(int argc,char *argv){
 
-    int i,k,n,m,vlen;
-    double *A,*v;
+//------ BubbleSort Paralelo -----
+int bubbleSort_Paralelo(double v[],int N){
+    int i=0, j=0; 
+    int inicio;
+    for(i=0;i<N-1;i++){
+	inicio = i % 2; 
+	#pragma omp parallel for default(none),shared(v,inicio,N)
+	for(j=inicio;j<N-1;j+=2){
+	    if(v[j] > v[j+1]){
+		intercambiar(&v[j],&v[j+1]);
+	    }
+	}
+    }      
+}
+
+
+
+int main(int argc,char *argv[]){
+    int i,N;
+    double *v,*u;
 
     //-- Variables de medición
     double start,end,T,Tsum;
 
-    //start = clock();
-    leerMatriz("Matriz.dat",&A,&m,&n);
-    leerVector("Vector.dat",&v,&vlen);
+    leerVector("Vector.dat",&v,&N);
+    leerVector("Vector.dat",&u,&N);
     
-    //-- Vector resultado
-    double *u = (double*) malloc(vlen*sizeof(double)); 
-    
-    Tsum = 0;
-    for(i=0;i<10;i++){
-        start = omp_get_wtime();
-        mxv_serial(m,n,A,v,u);
-        end = omp_get_wtime();
-        Tsum += end-start;
-    }
+    start = omp_get_wtime();
+    bubbleSort_Serial(v,N);
+    end = omp_get_wtime();
+    printf("T. de ejecución serial: %f s\n",end-start);
 
-    T = Tsum/20.0; //T. serial promedio 
-    printf("T. de ejecución serial: %f s\n",T);
-    
-    // Verificar resultado (solo para m pequeñas)
-    //for(i=0;i<n;i++)
-    //    printf("%f\n",u[i]);  
-    
-    Tsum = 0;
-    for(i=0;i<N;i++){
-        start = omp_get_wtime();
-        mxv_paralela(m,n,A,v,u);
-        end = omp_get_wtime();
-        Tsum += end - start;
-    }
-    
-    T = Tsum/20.0;
-    printf("T. de ejecución paralela: %f s\n",T); 
+    // Verificar ordenamiento (solo para N pequeñas)
+    //for(i=0;i<N;i++)
+    //    printf("%f\n",v[i]);
 
-    // Verificar resultado (solo para m pequeñas)
-    //for(i=0;i<n;i++)
-    //    printf("%f\n",u[i]);  
-}
-// -- Función de multiplicación Matriz-Vector serial
-void mxv_serial(int m, int n, double *A, double *v, double *u){
-    int i, j;
-    double parsum;
-    for(i=0; i<m; i++){
-        u[i] = 0.0;
-        for (j=0; j<n; j++)
-            u[i] += A[i*n+j]*v[j];
-    }
+    start = omp_get_wtime();
+    bubbleSort_Paralelo(u,N);
+    end = omp_get_wtime();
+    printf("T. de ejecución paralelo: %f s\n",end - start);
 
-}
+    // Verificar ordenamiento (solo para N pequeñas)
+    //puts("\n-- Paralelo -- \n");
+    //for(i=0;i<N;i++)
+    //    printf("%f\n",u[i]);   
 
-// -- Función de multiplicación Matriz-Vector paralela
-void mxv_paralela(int m, int n, double *A, double *v, double *u){
-    int i, j;
-    double parsum;
-  
-    #pragma omp parallel for private(j,parsum) 
-    for(i=0; i<m; i++){
-        parsum = 0.0;
-        for (j=0; j<n; j++)
-            parsum += A[i*n+j]*v[j];
-        u[i] = parsum;
-    }
 
+    return 0;
 }
